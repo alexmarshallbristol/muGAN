dfc28313d25708c8f3d483f669ff623a3602ee87
Alex Marshall <alex.marshall@bristol.ac.uk>
2020-05-20 23:22:03 +0100

    implement new seed arrays

--- Generation_Muon_Shield_Optimisation.py
+++ Generation_Muon_Shield_Optimisation.py
@@ -1,18 +1,12 @@
 '''
-
 	Script providing examples demonstrating how to use the GANs (Generative Adversarial Networks) developed 
 	for the fast generation of muon kinematics generated within SHiP target. 
-
 	Script introduces some of the functions avaliable in the module developed to clean up generation scripts.
-
 	The GAN employed here is an updated version of those presented in the SHiP paper: https://arxiv.org/abs/1909.04451
-
 	The new architecture introduces input auxiliary distributions. The generator has been taught to understand that these 
 	auxiliary inputs correspond to information about how rare each muon is in each direction. Varying the input auxiliary 
 	distributions can provide the user some control over the generated output.
-
-	Written by A. Marshall - April 2020.
-
+	Written by A. Marshall - May 2020.
 '''
 
 import numpy as np
@@ -26,9 +20,10 @@ muGAN = muGAN()
 
 
 ########################################################################################################################################################
-''' Load seed auxiliary distribution - file originally created with Create_seed_distribution.py'''
+''' Load seed auxiliary distribution '''
 seed_auxiliary_distributions = np.load('SHiP_GAN_module/data_files/Seed_auxiliary_values_for_enhanced_generation.npy')
-print(np.shape(seed_auxiliary_distributions))
+
+
 ########################################################################################################################################################
 '''
 	Although using these seed auxiliary distributions gets us most of the way we havent addressed the GAN underestimating the tails. 
@@ -37,7 +32,7 @@ print(np.shape(seed_auxiliary_distributions))
 	Feel free to play with this distribution.
 '''
 seed_auxiliary_distributions = np.take(seed_auxiliary_distributions,np.random.permutation(seed_auxiliary_distributions.shape[0]),axis=0,out=seed_auxiliary_distributions)
-fraction_to_boost = 0.11
+fraction_to_boost = 0.04
 cut = int(np.shape(seed_auxiliary_distributions)[0]*fraction_to_boost) 
 dist = np.abs(np.random.normal(loc=0,scale=1,size=np.shape(seed_auxiliary_distributions[:cut,2])))
 dist = np.abs(np.random.normal(loc=0,scale=1,size=np.shape(dist)))
@@ -45,10 +40,24 @@ dist += 1
 dist = np.power(dist,0.55)
 seed_auxiliary_distributions[:cut,2] *= dist
 seed_auxiliary_distributions = np.take(seed_auxiliary_distributions,np.random.permutation(seed_auxiliary_distributions.shape[0]),axis=0,out=seed_auxiliary_distributions)
+
+where_array = np.where(seed_auxiliary_distributions[:,3]>3)
+shape_i = int(np.shape(where_array)[1]*0.3)
+where_array = where_array[0][:int(shape_i)]
+seed_auxiliary_distributions[where_array,2] = seed_auxiliary_distributions[where_array,2]*1.4
+
+where_array = np.where(seed_auxiliary_distributions[:,3]>0)
+shape_i = int(np.shape(where_array)[1]*0.015)
+where_array = where_array[0][:int(shape_i)]
+seed_auxiliary_distributions[where_array,2] = seed_auxiliary_distributions[where_array,2]*1.3
+
+seed_auxiliary_distributions = np.take(seed_auxiliary_distributions,np.random.permutation(seed_auxiliary_distributions.shape[0]),axis=0,out=seed_auxiliary_distributions)
+
 ########################################################################################################################################################
 
 ''' Generate from GAN using auxiliary seed values. '''
-boosted_muon_kinematic_vectors = muGAN.generate_enhanced(auxiliary_distributions=seed_auxiliary_distributions, size=int(10000))
+boosted_muon_kinematic_vectors = muGAN.generate_enhanced(auxiliary_distributions=seed_auxiliary_distributions, size=int(5E4))
+
 
 ''' Plot the output. '''
 muGAN.plot_kinematics(data=boosted_muon_kinematic_vectors, filename='MSO_output/MSO_kinematics.png', normalize_colormaps=False)
@@ -56,6 +65,7 @@ muGAN.plot_p_pt(data=boosted_muon_kinematic_vectors,filename='MSO_output/MSO_P_P
 
 ''' Save to ROOT file. '''
 muGAN.save_to_ROOT(data=boosted_muon_kinematic_vectors,filename='MSO_output/MSO_muons.root')
+
 
 
 
